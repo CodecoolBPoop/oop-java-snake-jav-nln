ce01b8d3c1fa3cf29d084d131a7ad923065a3de4
Richard Hriech <hardric501@gmail.com>
2018-06-26 14:54:27 +0200

    restart ready.. again....

--- src/com/codecool/snake/Game.java
+++ src/com/codecool/snake/Game.java
@@ -1,6 +1,5 @@
 package com.codecool.snake;
 
-import com.codecool.snake.entities.enemies.SimpleEnemy;
 import com.codecool.snake.entities.powerups.SimplePowerup;
 import com.codecool.snake.entities.powerups.SuperPowerup;
 import com.codecool.snake.entities.snakes.SnakeHead;
@@ -14,20 +13,14 @@ import javafx.stage.Stage;
 
 public class Game extends Pane {
 
-    public static Text healthDisplay = new Text(50,50,"SNAKE 1 HEALTH: ");
-    public static Text healthDisplay2 = new Text(50,75,"SNAKE 2 HEALTH: ");
-    public static Text healthDisplay = new Text(50,50,"HEALTH: ");
-    public static Button restartButton = new Button("something");
+    public static Text healthDisplay = new Text(50, 50, "SNAKE 1 HEALTH: ");
+    public static Text healthDisplay2 = new Text(50, 75, "SNAKE 2 HEALTH: ");
+
     private Stage primaryStage;
 
     public Game(Stage primaryStage) {
         new SnakeHead(this, 500, 500);
-        new SnakeHead(this, 300,300);
-
-        new SimpleEnemy(this);
-        new SimpleEnemy(this);
-        new SimpleEnemy(this);
-        new SimpleEnemy(this);
+        new SnakeHead(this, 300, 300);
 
         new SimplePowerup(this);
         new SimplePowerup(this);
@@ -36,38 +29,58 @@ public class Game extends Pane {
 
         new SuperPowerup(this);
         new SuperPowerup(this);
+
+        restart();
 
         this.primaryStage = primaryStage;
     }
 
     public void restart() {
-        /*Image image = new Image("restart.png");
-        ImageView imageView = new ImageView(image);*/
-//        Button restartButton = new Button("asd");
-        /*restartButton.setLayoutX(10);
-        restartButton.setLayoutY(10);*/
-        Button btn = new Button();
-        btn.setText("'Hello World'");
-        this.getChildren().add(btn);
+        Image image = new Image("restart.png");
+        ImageView imageView = new ImageView(image);
+        Button restartButton = new Button("", imageView);
+        restartButton.setLayoutX(Globals.WINDOW_WIDTH - 60);
+        restartButton.setLayoutY(10);
+        this.getChildren().add(restartButton);
+        restartButton.setOnAction(ActionEvent -> {
+            primaryStage.close();
+            Main.initStage(primaryStage);
+        });
     }
 
     public void start() {
         Scene scene = getScene();
         scene.setOnKeyPressed(event -> {
             switch (event.getCode()) {
-                case LEFT:  Globals.leftKeyDown  = true; break;
-                case RIGHT: Globals.rightKeyDown  = true; break;
-                case A:  Globals.aKeyDown  = true; break;
-                case D: Globals.dKeyDown  = true; break;
+                case LEFT:
+                    Globals.leftKeyDown = true;
+                    break;
+                case RIGHT:
+                    Globals.rightKeyDown = true;
+                    break;
+                case A:
+                    Globals.aKeyDown = true;
+                    break;
+                case D:
+                    Globals.dKeyDown = true;
+                    break;
             }
         });
 
         scene.setOnKeyReleased(event -> {
             switch (event.getCode()) {
-                case LEFT:  Globals.leftKeyDown  = false; break;
-                case RIGHT: Globals.rightKeyDown  = false; break;
-                case A:  Globals.aKeyDown  = false; break;
-                case D: Globals.dKeyDown  = false; break;
+                case LEFT:
+                    Globals.leftKeyDown = false;
+                    break;
+                case RIGHT:
+                    Globals.rightKeyDown = false;
+                    break;
+                case A:
+                    Globals.aKeyDown = false;
+                    break;
+                case D:
+                    Globals.dKeyDown = false;
+                    break;
             }
         });
         Globals.gameLoop = new GameLoop();
